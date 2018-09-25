1f5ec25b334f1aab1d6180cf08795e23241d57ff
VcrTing <2782721850@qq.com>
2018-09-25 11:41:57 +0800

    first commit

---
+++ MintApi/MintApi/urls.py
@@ -0,0 +1,40 @@
+"""MintApi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import static, url
+
+from rest_framework import routers
+from rest_framework.documentation import include_docs_urls
+
+# Import
+# here that import you module
+from . import settings
+
+# Router
+# here setting your rest routers
+router = routers.DefaultRouter()
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('docs/', include_docs_urls(title='Mint外卖')),
+
+    path('', include(router.urls)),
+]
+
+urlpatterns += static.static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
